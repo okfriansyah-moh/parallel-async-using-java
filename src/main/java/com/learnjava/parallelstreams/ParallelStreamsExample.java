a08fa65471f26b677000f2c9bf9d07962ddfa298
okfriansyah <okfriansyah@doku.com>
2021-09-29 18:09:58 +0700

    add parallel stream example

---
+++ src/main/java/com/learnjava/parallelstreams/ParallelStreamsExample.java
@@ -0,0 +1,35 @@
+package com.learnjava.parallelstreams;
+
+import com.learnjava.util.DataSet;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ParallelStreamsExample {
+
+    public List<String> stringTransform(List<String> namesList) {
+        return namesList
+                .parallelStream()
+                .map(this::addNameLengthTransform)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        List<String> namesList = DataSet.namesList();
+        ParallelStreamsExample parallelStreamsExample = new ParallelStreamsExample();
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform(namesList);
+        log("result List : " + resultList);
+        timeTaken();
+    }
+
+    private String addNameLengthTransform(String name) {
+        delay(500);
+        return name.length() + "-" + name;
+    }
+}
